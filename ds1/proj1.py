be3b17a42ef66c2da8e343b9948093a6e607561f
Crimson Scythe <crimsonscythestudios@gmail.com>
2020-02-18 17:25:03 +0100

    works now - added required params for url and number replacement

--- ds1/proj1.py
+++ ds1/proj1.py
@@ -6,7 +6,7 @@ First, you will have to perform the following steps to create a clean version of
 
 1. Read the CSV file
 2. In the content field, do the following:
-- Tokenize and lower-case the text (end result should be an array of tokens where each token is a lexical unit or a punctuation). 
+- Tokenize and lower-case the text (end result should be an array of tokens where each token is a lexical unit or a punctuation).
   For example, “He said: ‘Don’t go there!’” => (he, said, :, ‘, don’t, go, there, !, ‘)
 - Remove consecutive spaces and new lines
 - Find and replace URLs with <URL>
@@ -33,20 +33,16 @@ from cleantext import clean
 
 sample_data = pd.read_csv("news_sample.csv")
 
-# We wanna make an array of arrays, with the tokenization of one content field, being in one array. 
-
+# We wanna make an array of arrays, with the tokenization of one content field, being in one array.
 
 array = []
 
 for i in range(len(sample_data)):
-	token_array = []
-	dirty_content = sample_data.at[i,'content']
-	dirty_content.replace("\n", " ")
-	clean_content = clean(dirty_content, lower=True, replace_with_url="<URL>", replace_with_number="<NUM>", fix_unicode=True)
-	token_array.append(clean_content)
-	array.append(token_array)
+    token_array = []
+    dirty_content = sample_data.at[i,'content']
+    # dirty_content.replace("\n", " ")
+    clean_content = clean(dirty_content, lower=True, no_urls = True, no_numbers=True, replace_with_url="<URL>", replace_with_number="<NUM>", fix_unicode=True)
+    token_array.append(clean_content)
+    array.append(token_array)
 
 print(array[1])
-
-
-
