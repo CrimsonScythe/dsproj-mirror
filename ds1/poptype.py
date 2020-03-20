643744256fdcc9acd355dd5a93047481fa9f5f41
Crimson Scythe <crimsonscythestudios@gmail.com>
2020-03-20 10:59:05 +0100

    changed poptype

--- ds1/poptype.py
+++ ds1/poptype.py
@@ -18,14 +18,66 @@ import math
 
 var = 'NULL'
 # load data
-sample_data = pd.read_csv("1mio-raw.csv", usecols = ['type'])
 
-sample_data = sample_data.replace(np.nan, var, regex=True)
+def chunk_preprocessing(sample_data):
+          
+    """
+    skip the row if id is not an int
+    this occured at one point
+    """          
+    bol=pd.to_numeric(sample_data['id'], errors='coerce').notnull().all()
+    if (bol == False):
+        return None
+
+    return sample_data    
+
+df_chunk = pd.read_csv("1mio-raw.csv", chunksize=2000, usecols = ['id', 'type'])
+
+
+chunk_list = []
+"""
+chunksize of 2000 was optimal for my system
+"""
+# chunksize = 2000
+
+col_names =  ['id', 'domain', 'type', 'url', 'content', 'scraped_at', 'inserted_at', 'updated_at', 'title', 'authors', 'keywords', 'meta_keywords', 'meta_description', 'tags', 'summary']
+# df_chunk = pd.read_csv("1mio-raw.csv", chunksize=chunksize, usecols=col_names, low_memory=True)
+
+array = []
+
+col_names =  ['id', 'domain', 'type', 'url', 'content', 'scraped_at', 'inserted_at', 'updated_at', 'title', 'authors', 'keywords', 'meta_keywords', 'meta_description', 'tags', 'summary']
+
+for chunk in df_chunk:
+    
+    chunk_filter = chunk_preprocessing(chunk)
+
+    if (chunk_filter is None):
+        continue
+ 
+    # i=i+1
+
+    """
+    print progress
+    """
+    # if i % 2 == 0:
+        # print(i)
+
+    chunk_list.append(chunk_filter)
+    
+
+df = pd.concat(chunk_list)   
+
+
+# sample_data = sample_data.replace(np.nan, var, regex=True)
 
 
 # cleaned data is converted to CSV
 # 
-sample_data.to_csv('type.csv', index=True, header=False)
+val = df['id'].to_frame().join(df['type'])
+val.to_csv('type.csv', index=False, header=False)
+
+
+
 
 # CSV is opened so it can be copied
 f = open('type.csv', encoding="utf8")
@@ -33,6 +85,6 @@ f = open('type.csv', encoding="utf8")
 # writing to DB
 conn = psycopg2.connect(host = "localhost", dbname="postgres", user="postgres", password="root")
 cur = conn.cursor() 
-cur.copy_from(f, 'type', sep=',')
+cur.copy_from(f, 'article_type', sep=',')
 conn.commit()
 cur.close()
