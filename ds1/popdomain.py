cda0ecc3b40922e79d18d1fbbf19115bd00b3f1a
Crimson Scythe <crimsonscythestudios@gmail.com>
2020-03-20 17:39:04 +0100

    gave it the same treatment as type. basically it now uses the same id as article

--- ds1/popdomain.py
+++ ds1/popdomain.py
@@ -18,15 +18,65 @@ import math
 
 var = 'NULL'
 # load data
-sample_data = pd.read_csv("1mio-raw.csv", usecols = ['domain'])
 
-# sample_data_cleaned = sample_data.replace(np.nan, None, regex=True)
-sample_data = sample_data.replace(np.nan, var, regex=True)
+def chunk_preprocessing(sample_data):
+          
+    """
+    skip the row if id is not an int
+    this occured at one point
+    """          
+    bol=pd.to_numeric(sample_data['id'], errors='coerce').notnull().all()
+    if (bol == False):
+        return None
+
+    return sample_data    
+
+df_chunk = pd.read_csv("1mio-raw.csv", chunksize=2000, usecols = ['id', 'domain'])
 
 
+chunk_list = []
+"""
+chunksize of 2000 was optimal for my system
+"""
+# chunksize = 2000
+
+col_names =  ['id', 'domain', 'type', 'url', 'content', 'scraped_at', 'inserted_at', 'updated_at', 'title', 'authors', 'keywords', 'meta_keywords', 'meta_description', 'tags', 'summary']
+# df_chunk = pd.read_csv("1mio-raw.csv", chunksize=chunksize, usecols=col_names, low_memory=True)
+
+array = []
+
+col_names =  ['id', 'domain', 'type', 'url', 'content', 'scraped_at', 'inserted_at', 'updated_at', 'title', 'authors', 'keywords', 'meta_keywords', 'meta_description', 'tags', 'summary']
+
+for chunk in df_chunk:
+    
+    chunk_filter = chunk_preprocessing(chunk)
+
+    if (chunk_filter is None):
+        continue
+ 
+    # i=i+1
+
+    """
+    print progress
+    """
+    # if i % 2 == 0:
+        # print(i)
+
+    chunk_list.append(chunk_filter)
+    
+
+df = pd.concat(chunk_list)   
+
+
+# sample_data = sample_data.replace(np.nan, var, regex=True)
+
+
+# cleaned data is converted to CSV
 # 
-# sample_data.drop(sample_data.index[[116454]], inplace=True)
-sample_data.to_csv('domain.csv', index=True, header=False)
+val = df['id'].to_frame().join(df['domain'])
+val.to_csv('domain.csv', index=False, header=False)
+
+
 
 
 # CSV is opened so it can be copied
