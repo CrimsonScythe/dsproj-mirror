519a0f1b8d61a2bb1138f8ad999d343fb0fc9d9c
Crimson Scythe <crimsonscythestudios@gmail.com>
2020-03-28 16:40:35 +0100

    updated task 3.3 so it now uses the tags and keywords tables as we cannot use meta_keywords in article table

--- ds1/queries.sql
+++ ds1/queries.sql
@@ -11,7 +11,23 @@ article_type.type_name='fake' AND NOT author_name=''
 GROUP BY author_name
 ORDER BY COUNT(author_name) DESC;
 --3. 
-SELECT COUNT(article_id), meta_keywords FROM article
-WHERE NOT meta_keywords='[]' AND NOT meta_keywords='[ ]'
-GROUP BY meta_keywords
+-- VERSION 1: (deprecated)
+-- SELECT COUNT(article_id), meta_keywords FROM article
+-- WHERE NOT meta_keywords='[]' AND NOT meta_keywords='[ ]'
+-- GROUP BY meta_keywords
+-- ORDER BY COUNT(article_id) DESC;
+-- VERSION 2: (also deprecated)
+-- SELECT COUNT (article_id), keyword_id FROM tags
+-- GROUP BY keyword_id
+-- ORDER BY COUNT(article_id) DESC
+
+-- VERSION 3: (in prod :D)
+-- Needed to update this query since meta_keywords from article table cannot be used.
+-- Thus, here we use the tags and keywords table to find the required info.
+SELECT COUNT (article_id), keyword_id 
+FROM (
+SELECT * FROM tags
+WHERE tags.keyword_id != (SELECT keyword.keyword_id FROM keyword WHERE keyword.keyword = 'NULL')
+) AS foo
+GROUP BY keyword_id
 ORDER BY COUNT(article_id) DESC;
