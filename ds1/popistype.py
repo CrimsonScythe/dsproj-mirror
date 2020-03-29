f55a2af642455862f4b5c54e7270b156d98174b0
Crimson Scythe <crimsonscythestudios@gmail.com>
2020-03-29 18:11:28 +0200

    poptype now contains unique values only. popistype populats the is_type table

--- ds1/popistype.py
+++ ds1/popistype.py
@@ -1,3 +1,12 @@
+import pandas as pd
+import numpy as np
+from cleantext import clean
+from datetime import datetime
+import datefinder
+import re
+import csv
+import psycopg2
+
 
 """
 (a) For each table, devise a script employing the text cleaning library to clean the data
@@ -16,26 +25,48 @@ import csv
 import psycopg2
 import math
 
+i=0
 var = 'NULL'
 # load data
 
-def chunk_preprocessing(sample_data):
+def chunk_preprocessing(sample_data, dict):
           
     """
     skip the row if id is not an int
     this occured at one point
     """          
-    # bol=pd.to_numeric(sample_data['id'], errors='coerce').notnull().all()
-    # if (bol == False):
-        # return None
+    bol=pd.to_numeric(sample_data['id'], errors='coerce').notnull().all()
+    if (bol == False):
+        return None
 
-    # First we replace any weird entries with 'NULL' and to save headaches later on when querying, we skip over the nulls so our db doesnt contain any nulls at all. 
+    if (pd.isnull(sample_data['type'].all())):
+        return None
+    
+    
 
-    sample_data['type'].replace(to_replace=np.nan, value='NULL', regex=True, inplace=True)
+    sample_data['type'].replace(dict, inplace=True)
+
+    # for index, row in sample_data.iterrows():
+    #     if (pd.isnull(row['type'])):
+    #         continue
+    #     row['type'] = dict[row['type']]
+        # print(row['id'], row['type'])
+
+    # print(sample_data)
+    
 
     return sample_data    
 
-df_chunk = pd.read_csv("1mio-raw.csv", chunksize=2000, usecols = ['type'])
+conn1 = psycopg2.connect(host = "localhost", dbname="postgres", user="postgres", password="root")
+cur1 = conn1.cursor() 
+cur1.execute('SELECT * FROM article_type;')
+types = cur1.fetchall()
+temp_dict = dict(types)
+# swap keys and values
+my_dict = dict((v,k) for k,v in temp_dict.items())
+
+
+df_chunk = pd.read_csv("1mio-raw.csv", chunksize=2000, usecols = ['id', 'type'])
 
 
 chunk_list = []
@@ -53,36 +84,32 @@ col_names =  ['id', 'domain', 'type', 'url', 'content', 'scraped_at', 'inserted_
 
 for chunk in df_chunk:
     
-    chunk_filter = chunk_preprocessing(chunk)
+    chunk_filter = chunk_preprocessing(chunk, my_dict)
 
     if (chunk_filter is None):
         continue
- 
-    # i=i+1
-
-    """
-    print progress
-    """
-    # if i % 2 == 0:
-        # print(i)
+    
+    i=i+1
+    
+    if i % 2 == 0:
+        print(i)
 
     chunk_list.append(chunk_filter)
     
 
 df = pd.concat(chunk_list)   
-
-df.drop_duplicates(subset=['type'], keep='first', inplace=True)
-
-df.to_csv('type.csv', index=True, header=False)
-
+df['type'] = df['type'].fillna(397)
+df['type'] = df['type'].astype(int)
+df.to_csv('is_type.csv', index=False, header=False)
 
 
+print('5')
 # CSV is opened so it can be copied
-f = open('type.csv', encoding="utf8")
+f = open('is_type.csv', encoding="utf8")
 
 # writing to DB
 conn = psycopg2.connect(host = "localhost", dbname="postgres", user="postgres", password="root")
 cur = conn.cursor() 
-cur.copy_from(f, 'article_type', sep=',')
+cur.copy_from(f, 'is_type', sep=',')
 conn.commit()
 cur.close()
