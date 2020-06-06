70d16e1abb46c958e12d1a2f36f6dbb8fef6db77
Crimson Scythe <crimsonscythestudios@gmail.com>
2020-06-06 21:56:00 +0200

    rafactored baslines and datascript

--- ds1/predictions/baselines.py
+++ ds1/predictions/baselines.py
@@ -1,148 +1,143 @@
 import pandas as pd
-import numpy as np
-import csv
-from limit_dataset import LimitDataSet
+from sklearn import linear_model
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn import linear_model
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import AdaBoostClassifier
-
-df = LimitDataSet.createDataFrame(1000)
 
 
-# we need to explicitly convert the type ids to ints
-Y = pd.to_numeric(df['type_id'])
-X = df['content']
+class Baselines:
+    def __init__(self):
+        super().__init__()
+    
+        # prepare datasets
+        dfTrain = pd.read_csv("train.csv")
+        dfTest = pd.read_csv("valid.csv")
+        dfliar = pd.read_csv("test.tsv", sep='\t')
+        dfkaggle = pd.read_json("test_set.json")
 
-count_vect = CountVectorizer(ngram_range=(1,2))
-X_train_counts = count_vect.fit_transform(X)
-print(X_train_counts.shape)
+        X_train = dfTrain['content']
+        y_train = pd.to_numeric(dfTrain['type_id'])
 
-tf_transformer = TfidfTransformer().fit(X_train_counts)
-X = tf_transformer.transform(X_train_counts)
+        X_test_corp = dfTest['content']
+        y_test_corp = pd.to_numeric(dfTest['type_id'])
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=1)
+        X_test_liar = dfliar.iloc[:, 2].to_numpy()
+        y_test_liar = dfliar.iloc[:, 1]
 
-####################
-# SVM CLASSIFIER WITH LINEAR KERNEL
+        X_test_kaggle = dfkaggle.iloc[: , 1].to_numpy()
+        ids_kaggle = dfkaggle.iloc[:, 0]
+        
+        self.X_train = self.vectorizeX(X_train)
+        self.y_train = y_train
 
-SVM = linear_model.SGDClassifier(max_iter=1000, tol=1e-3)
-SVM.fit(X_train, y_train)
-score = SVM.score(X_test, y_test)
+        self.X_test_corp = self.vectorizeX(X_test_corp)
+        self.y_test_corp = y_test_corp
 
-print("SVM score "+str(score))
-# parameters = {'loss':('hinge', 'log', 'modified_huber'),
-#  'penalty':('l2', 'l1', 'elasticnet')}
-# clf = GridSearchCV(estimator=linear_model.SGDClassifier(), param_grid=parameters, n_jobs=-1)
-# clf.fit(X_train, y_train)
-# print("SVM best score" + str(clf.best_score_))
-# print("best params" + str(clf.best_params_))
-####################
+        self.X_test_liar = self.vectorizeX(X_test_liar)
+        self.y_test_liar = y_test_liar.map(self.encodeLiar)
 
-####################
-# SVM CLASSIFIER WITH NON-LINEAR KERNEL
+        self.X_test_kaggle = self.vectorizeX(X_test_kaggle)
+        self.ids_kaggle = ids_kaggle
 
-# SVC = SVC(C=1.0, kernel='rbf', degree=4, gamma='scale', coef0=0.0)
-# SVC.fit(X_train, y_train)
-# score = SVC.score(X_test, y_test)
+        # prepare models
+        self.SVM = linear_model.SGDClassifier(max_iter=1000, tol=1e-3)
+        self.NN = KNeighborsClassifier(n_neighbors=5)
+        self.MNB = MultinomialNB()
+        self.DT = DecisionTreeClassifier(max_depth=None)
 
-# print("SVC score" + str(score))
+    def vectorizeX(self, input):
 
-# parameters = {'C':[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0], 'kernel':('rbf', 'poly', 'sigmoid'),
-#  'degree':[3], 'gamma':('scale', 'auto')}
-# clf = GridSearchCV(estimator=SVC(), param_grid=parameters, n_jobs=-1)
-# clf.fit(X_train, y_train)
-# print("SVC best score" + str(clf.best_score_))
-# print("best params" + str(clf.best_params_))
+        count_vect = CountVectorizer(ngram_range=(1,2))
+        input_train_counts = count_vect.fit_transform(input)
 
-# parameters = {'kernel':('rbf', 'poly', 'sigmoid')}
-# clf = GridSearchCV(estimator=SVC(), param_grid=parameters, n_jobs=-1)
-# clf.fit(X_train, y_train)
-# print("SVC best score" + str(clf.best_score_))
-# print("best params" + str(clf.best_params_))
+        tf_transformer = TfidfTransformer().fit(input_train_counts)
+        return tf_transformer.transform(input_train_counts)
 
+    # 56% acc. if half-true is encoded as 1, 35% otherwise
+    def encodeLiar(self, y):
+        if (y=="true" or y=="mostly-true") or y=="half-true":
+            return 1
+        else:
+            return 0
 
-####################
+    def __train(self, model, Xtest):
 
-####################
-# NN classifier
+      # we need to either append 0 columns to the test set,
+        # so it matches the size of the training set
+        # but that does not scale as the dataset is large,
+        # so we truncate the training set instead.
 
-# default n value is also 5
-# NN = KNeighborsClassifier(n_neighbors=5)
-# NN.fit(X_train, y_train)
-# score = NN.score(X_test, y_test)
+        print(self.X_train.shape)
+        print(Xtest.shape)
 
+        trunc = self.X_train[:, :Xtest.shape[1]]
 
+        print(trunc.shape)
 
-# print("NN score "+str(score))
-# parameters = {'n_neighbors':[1,2,3,4,5], 'weights':('uniform', 'distance')}
-# clf = GridSearchCV(estimator=KNeighborsClassifier(), param_grid=parameters, n_jobs=-1)
-# clf.fit(X_train, y_train)
-# print("NN best score" + str(clf.best_score_))
-# print("best params" + str(clf.best_params_))
-####################
+        model.fit(trunc, self.y_train)
 
-# ####################
-# # NB classifier
+    
+    def test_corp(self, modelName: str, model):
 
-# # this is Multinomial Naive Bayes (Gaussian did not work as it required a dense matrix whereas the matrix we have is sparse
-# # also it looks like Multinomial is better for text classification according to GHUB and SO)
-# # there are also other variants which are perhaps better
-# MNB = MultinomialNB() 
-# MNB.fit(X_train, y_train)
-# score = MNB.score(X_test, y_test)
+        # train on corp
+        # truncating is handled by self.train see above
+        self.__train(model, self.X_test_corp)
 
-# print("MNB score "+str(score))
-# parameters = {'alpha':[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0], 'fit_prior':('True', 'False')}
-# clf = GridSearchCV(estimator=MultinomialNB(),
-#  param_grid=parameters, n_jobs=-1)
-# clf.fit(X_train, y_train)
-# print("MNB best score" + str(clf.best_score_))
-# print("best params" + str(clf.best_params_))
+        score = model.score(self.X_test_corp, self.y_test_corp)
+        print(modelName+'corpus accuracy:'+str(score))
 
+    def test_liar(self, modelName: str, model):
 
-####################
+        self.__train(model, self.X_test_liar)
 
-####################
-# DT classifier
+        score = model.score(self.X_test_liar, self.y_test_liar)
+        print(modelName+'liar accuracy:'+str(score))
 
+    def decodeKaggle(self, x):
+        if (x==0):
+            return 'FAKE'
+        else:
+            return 'REAL'
 
-# DT = DecisionTreeClassifier(max_depth=None) 
-# DT.fit(X_train, y_train)
-# score = DT.score(X_test, y_test)
+    def test_kaggle(self, modelName: str, model):
 
-# print("DT score "+str(score))
-# parameters = {'criterion':('gini', 'entropy'), 'splitter':('best', 'random')}
-# clf = GridSearchCV(estimator=DecisionTreeClassifier(),
-#  param_grid=parameters, n_jobs=-1)
-# clf.fit(X_train, y_train)
-# print("DT best score" + str(clf.best_score_))
-# print("best params" + str(clf.best_params_))
+        self.__train(model, self.X_test_kaggle)
 
+        predictions = model.predict(self.X_test_kaggle)
+        tr=0
+        fl=0
+        for x in predictions:
+            if (x==0):
+                fl+=1
+            else:
+                tr+=1
+        print("reals"+str(tr))
+        print("fakes"+str(fl))     
 
+        labels = pd.DataFrame(data=predictions, columns=['label']).applymap(self.decodeKaggle)
+        print(labels)
 
-# ####################
+        self.ids_kaggle = self.ids_kaggle.to_frame()
+        self.ids_kaggle.columns = ['id']
 
-# ####################
+        kaggle_res = self.ids_kaggle.join(labels)
 
-# clf = AdaBoostClassifier(n_estimators=1000)
-# clf.fit(X_train, y_train)
-# score = clf.score(X_test, y_test)
-
-# print("SCORE"+str(score))
-# ####################
+        kaggle_res.to_csv('predictions.csv', index=False)
 
 
-# ####################
-# # Using grid search here
+'''
+Note.
+1.The string you pass for the model name can be anything.
+It is only used in print statements.
+2. Remember to pass the model from the instantiated class itself. i.e. baselines.SVM
+'''
 
-# GridSearchCV
-
-# ####################
+'''
+Example usage below
+'''
+baselines = Baselines()
+baselines.test_corp("SVM", baselines.SVM)
+baselines.test_liar("SVM", baselines.SVM)
+baselines.test_kaggle("SVM", baselines.SVM)
--- ds1/predictions/datascript.py
+++ ds1/predictions/datascript.py
@@ -1,15 +1,15 @@
 from limit_dataset import LimitDataSet
 import pandas as pd
 
-df = LimitDataSet.createDataFrame(5000)
-
-def func(x):
-    # pass
-    if (x=="true" or x=="mostly-true"):
-    #     print("yes")
-        return 1
-    elif(x=="false" or x=="barely-true" or x=="pants-fire"  or x=="half-true"):
-        return 0
+df = LimitDataSet.createDataFrame(7000)
+#
+# def func(x):
+#     # pass
+#     if (x=="true" or x=="mostly-true"):
+#     #     print("yes")
+#         return 1
+#     elif(x=="false" or x=="barely-true" or x=="pants-fire"  or x=="half-true"):
+#         return 0
 
 
 # print(df.shape[0])
@@ -30,8 +30,8 @@ df_valid = df.iloc[int(train_size)+int(test_size):, :]
 df_train.to_csv('train.csv', index=False)
 df_test.to_csv('test.csv', index=False)
 df_valid.to_csv('valid.csv', index=False)
-
-df_liar = pd.read_csv('test.tsv', sep='\t')
+#
+# df_liar = pd.read_csv('test.tsv', sep='\t')
 # Xd = df_liar.iloc[:,2]
 # Yd = df_liar.iloc[:,1]
 
@@ -42,3 +42,9 @@ df_liar = pd.read_csv('test.tsv', sep='\t')
 #
 # print(X_arr)
 # print(Y_arr)
+# df = pd.read_json("test_set.json")
+# print(df)
+# Xd = df.iloc[: , 0]
+# print(Xd)
+# X_arr = Xd.to_numpy()
+# print(X_arr.shape)
